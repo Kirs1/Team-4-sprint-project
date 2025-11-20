2b7ecfbf1d5a9f95183adec0b7af926218f3b647
winter-nya <wlou6789@bu.edu>
2025-11-20 12:35:27 -0500

    removed unused code

--- spark-bytes/src/components/eventcard.tsx
+++ spark-bytes/src/components/eventcard.tsx
@@ -1,18 +1,6 @@
 "use client";
 
 import { Card } from "antd";
-import {Typography} from "antd";
-
-const {Text, Paragraph} = Typography;
-import styles from "../styles/eventcard.module.css";
-
-
-// interface EventCardProps {
-//   title: string;
-//   date: string;
-//   location: string;
-//   description: string;
-// }
 
 export default function EventCard({ name, start_time, end_time, location_name, organizations }: any) {
   const start = start_time ? new Date(start_time).toLocaleString() : "N/A";
@@ -27,25 +15,3 @@ export default function EventCard({ name, start_time, end_time, location_name, o
     </Card>
   );
 }
-
-//   return (
-//     <Card
-//       title={title}
-
-//       className={styles.card}
-//       headStyle={{
-      
-//         color: 'blue', 
-        
-//       }}
-//       style={{
-//         borderRadius: 12,
-//         height: "100%",
-//       }}
-//     >
-//       <p className={styles.date}><strong>Date:</strong> {date}</p>
-//       <p className={styles.location}><strong>Location: </strong>{location}</p>
-//       <p>{description}</p>
-//     </Card>
-//   );
-// }
