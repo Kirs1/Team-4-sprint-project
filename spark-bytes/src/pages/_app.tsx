e0a23f8d7a89ce12693e706c5496736bf9afd5b4
winter-nya <wlou6789@bu.edu>
2025-11-20 12:25:15 -0500

    switched to next-auth

--- spark-bytes/src/pages/_app.tsx
+++ spark-bytes/src/pages/_app.tsx
@@ -1,11 +1,13 @@
-import { AuthProvider } from '../../contexts/authcontext';
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <AuthProvider>
+    <SessionProvider session={session}>
       <Component {...pageProps} />
-    </AuthProvider>
+    </SessionProvider>
   );
 }
