0c58b85ebaaaa01baf659547ef4e5eabab977a27
winter-nya <wlou6789@bu.edu>
2025-11-22 13:15:05 -0500

    moved auth to pages router

---
+++ spark-bytes/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+
+  callbacks: {
+    async signIn({ user }) {
+      return true;
+    },
+    async session({ session, user, token }) {
+      return session;
+    },
+  },
+});
